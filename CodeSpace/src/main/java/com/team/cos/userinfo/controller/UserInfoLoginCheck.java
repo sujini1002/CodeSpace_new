68caf301df93c2b0a7b767d9f689657b077bfbd6
HJS <HJS@DESKTOP-75SVTF7>
2018-11-19 15:13:17 +0900

    18.11.19 아이디 기억 추가(쿠키)(지승)

--- CodeSpace/src/main/java/com/team/cos/userinfo/controller/UserInfoLoginCheck.java
+++ CodeSpace/src/main/java/com/team/cos/userinfo/controller/UserInfoLoginCheck.java
@@ -1,14 +1,15 @@
 package com.team.cos.userinfo.controller;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.team.cos.userinfo.service.UserInfoLoginUpdateService;
 import com.team.cos.userinfo.vo.UserInfoVo;
@@ -21,30 +22,34 @@ public class UserInfoLoginCheck {
 	private UserInfoLoginUpdateService updateService;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String getUserInfoLogin() {
+	public String getUserInfoLogin(HttpServletRequest request, HttpServletResponse response) {
+		Cookie[] cookie = request.getCookies();
 		return "userinfo/userlogin";
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
 	@ResponseBody
-	public int postUserInfoLogin(UserInfoVo userInfoVo,HttpSession session) {
+	public int postUserInfoLogin(UserInfoVo userInfoVo, HttpSession session, boolean user_cookie,
+			HttpServletResponse response) {
 		UserInfoVo result = new UserInfoVo();
 		result = updateService.userLoginCheck(userInfoVo);
-		
-		System.out.println(userInfoVo);
-		System.out.println(result);
-		
+
 		if (result.getUser_pw() == null) {
 //			pw 가 없으면 구글 로그인
 			return 1;
-		} else if (userInfoVo.getUser_id().equals(result.getUser_id()) && userInfoVo.getUser_pw().equals(result.getUser_pw())) {
+		} else if (userInfoVo.getUser_id().equals(result.getUser_id())
+				&& userInfoVo.getUser_pw().equals(result.getUser_pw())) {
 //			사용자 제대로 확인 되었을떄
+//			쿠키가 있을때 쿠키 저장후 response
+			if (user_cookie == true) {
+				Cookie cookie = new Cookie("user_id", result.getUser_id());
+				response.addCookie(cookie);
+			}
 			updateService.userLoginUpdate(userInfoVo);
 			session.setAttribute("loginInfo", result);
 			return 2;
-		}else {
+		} else {
 			return 0;
 		}
-
 	}
 }
