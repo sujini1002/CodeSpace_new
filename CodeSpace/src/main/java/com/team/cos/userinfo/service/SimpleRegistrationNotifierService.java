239f20a57182bb43e15c49d00b18f9e40452a40b
HJS <HJS@DESKTOP-75SVTF7>
2018-11-20 10:02:34 +0900

    18.11.20 이메일 css 수정(지승)

--- CodeSpace/src/main/java/com/team/cos/userinfo/service/SimpleRegistrationNotifierService.java
+++ CodeSpace/src/main/java/com/team/cos/userinfo/service/SimpleRegistrationNotifierService.java
@@ -22,7 +22,7 @@ public class SimpleRegistrationNotifierService {
 		String homePage = "<div style=\"height:300px; margin:15% auto; width:20%\">\r\n"
 				+ "        <h1 style=\"text-align:center;margin-bottom: 50px\">CODE SPACE</h1>\r\n"
 				+ "        <div style=\"text-align:center; font-size: 17px;\">회원가입을 진심으로 감사드립니다!!</div>\r\n"
-				+ "        <button style=\"border: 0px; margin: 10% 21%; height: 50px; width: 300px; align-items: center; border-radius: 5px; background-color: #007bff; border-color: #007bff; font-size: 20px; color: white; cursor: pointer;\"><a href=\"\" style=\"color: white; text-decoration: none\">홈페이지로 이동하기</a></button>\r\n"
+				+ "        <button style=\"border: 0px; margin: 10% 2%; height: 50px; width: 300px; align-items: center; border-radius: 5px; background-color: #007bff; border-color: #007bff; font-size: 20px; color: white; cursor: pointer;\"><a href=\"http://localhost:8080/cos/\" style=\"line-height:50px; color: white; text-decoration: none\">홈페이지로 이동하기</a></button>\r\n"
 				+ "        <p style=\"text-align: center;color: gray;\">&copy; 2018-2019</p>\r\n" + "    </div>";
 		try {
 			/* message.setSubject("회원가입을 진심으로 감사합니다."); */
